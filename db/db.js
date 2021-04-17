5b4fa66066ca875f46ab4179de9c845cbcacc012
amata7 <adrianmata@live.com>
2021-04-17 00:19:25 -0700

    added db.js file in db

---
+++ db/db.js
@@ -0,0 +1,9 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const readNotes = () => 
+    fs
+        .readFile(path.join(__dirname, './db.json'), 'utf-8')
+        .then((data) => JSON.parse(data));
+
+modeule.exports = {readNotes};
--- server.js
+++ server.js
@@ -2,15 +2,22 @@ const express = require('express');
 const path = require('path');
 const db = require('./db');
 
+// Sets up the Express App
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 app.use (
     express.static(path.join(__dirname, './public'), { extensions: ['.html'] } )
 );
 
-app.get('/api/movies', (req,res) => {
-    db.readMovies().then((movies) => res.json(movies))
+app.get('/api/note', (req,res) => {
+    db.readNote().then((note) => res.json(note))
     });
 
 app.listen(PORT, () => console.log(`App Listening on PORT ${PORT}`));
